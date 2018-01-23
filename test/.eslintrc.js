de814db875194e7b0b0f97b197c1ac9de509fe6f
Austin Murdock <austin.d.murdock@gmail.com>
2018-01-23 14:41:44 -0600

    New: config files and updates to align with those files

---
+++ test/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'env': {
+    'mocha': true,
+  },
+  'plugins': [
+    'chai-expect',
+  ],
+  'globals': {
+    'expect': false,
+    'fixture': false,
+    'WCT': false,
+  },
+};
