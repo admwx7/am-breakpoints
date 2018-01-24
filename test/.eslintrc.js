9713b02b4a18bec0e27dd095815dcd0ec01b2c65
Austin Murdock <austin.d.murdock@gmail.com>
2018-01-24 11:36:29 -0600

    New: unit tests

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -8,6 +8,12 @@ module.exports = {
   'globals': {
     'expect': false,
     'fixture': false,
+    'flush': false,
+    'replace': false,
+    'sinon': false,
     'WCT': false,
+  },
+  rules: {
+    'no-unused-expressions': 0,
   },
 };
